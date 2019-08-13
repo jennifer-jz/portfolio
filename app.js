2b486a919817d5cab2bcb2534cde20a67550678a
JJ <jesczhao@gmail.com>
2019-08-13 00:31:17 -0700

    refactor appiis.js

--- app.js
+++ app.js
@@ -9,37 +9,15 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var constants = require('./shared/constants');
-
 var app = express();
 
-var fs = require('fs'), ini = require('ini'); 
-
-var conf = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-
-var { common } = conf['section'];
-var { version = '' } = common;
-
-var rootSubPath = version ? '/' + version : '';
-var rootpath = common.url+":"+common.port + rootSubPath;
-
-var html = {
-  rootpath,
-  rootSubPath,
-  ...constants,
-};
-
-$arr = {
-  ...conf['section'],
-  common: { ...common, path: __dirname },
-  html,
-}
+require('./global');
+var { rootSubPath } = $arr.html;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -59,11 +37,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
